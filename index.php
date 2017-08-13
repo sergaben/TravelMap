5ca1dbaaba36298248747d0e95b86d40c3243b2c
sergaben <gabense@hotmail.com>
2017-08-13 15:29:22 +0100

    new features and fixes

--- index.php
+++ index.php
@@ -1,51 +1,69 @@
-<?php 
-if(isset($_SERVER['HTTPS']))
-{
-    $_SERVER['HTTP_HTTPS'] = 0;
-}
-?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Portfolio</title>
-        <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-        <link rel="manifest" href="favicon/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
-        <meta name="theme-color" content="#ffffff">
-
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-       
+
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css" integrity="sha256-itWEYdFWzZPBG78bJOOiQIn06QCgN/F0wMDcC4nOhxY=" crossorigin="anonymous" />
 
         <link type="text/css" rel="stylesheet" href="css/main.css">
     </head>
     <body>
-       
-        <div id="Modal-Instructions" class ="modal">
-            <div class="modal-content">
-                <span class="close">&times;</span>
-                <p>Right click for select the country that you want to highlight</p> 
+        <div id="sideNavigation" class="sidenav" style="width:0%;">
+            <a>My Projects</a>
+            <a>About me</a>
+            <a>More options</a>
+        </div>
+        <span id="rightarrow" onclick="toggleState()" class="openNav"> <i id="arrow" class="right"></i></span>
+
+        <div id="mainContent">
+            <div id="Modal-Instructions" class ="modal">
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <p>Right click for select the country that you want to highlight</p> 
+                    <p>    Right click again to unselect the country</p>
+                </div>
+            </div>
+
+            <object id='map' class="worldMap"></object>
+            <div id="legend" class="legend">
+                <h3>
+                    Legend
+                </h3>
+                <div id="visitedCountry" class="visitedCountry"><label class="inline">Visited countries</label></div>
+                <div id="unvisitedCountry" class="unvisitedCountry"><label class="inline">Unvisited countries</label></div>
+                <div id="wantToVisitCountry" class="wantToVisitCountry"><label class="inline">Want to visit countries</label></div>
             </div>
         </div>
-        <div id='map' class="worldMap"></div>
-        <h3 id="region-name"></h3>
-        <button id='saveButton'>Save</button>
+        <script> 
+            // Variables for the manipulation of the DOM
+            var sideNavigation = document.getElementById("sideNavigation");
+            var mainContent = document.getElementById("mainContent");
+            var rightArrow = document.getElementById("rightarrow");
+            var arrow = document.getElementById("arrow");
+            // Function that will close and open the side navigation menu
+            function toggleState(){
+                if($("#sideNavigation").width() === 0)
+                {
+                    sideNavigation.style.width = "250px";
+                    mainContent.style.marginLeft = "250px";
+                    rightArrow.style.marginLeft = "268px";
+                    $("#arrow").removeClass('right');
+                    $("#arrow").addClass('left');
+                }else{
+                    sideNavigation.style.width = "0";
+                    mainContent.style.marginLeft = "0";
+                    rightArrow.style.marginLeft = "0";
+                    $("#arrow").removeClass('left');
+                    $("#arrow").addClass('right');
+                }
+            }
+
+        </script>
         <script src="raphael.min.js"></script>
+        <script src="raphael-svg-import.js"></script>
         <script src="world.js?v=789"></script>
-        <script src="scripts.js"></script>
+        <script src="scripts.js?v=230"></script>
     </body>
 </html>
--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
+//Function to show the modal the document is ready.
 $(document).ready(function(){
     var modal = document.getElementById("Modal-Instructions");
 
-    var span = document.getElementsByClassName("Close")[0];
+    var span = document.getElementsByClassName("close")[0];
     
     modal.style.display = "block";
     
     span.onclick = function(){
         modal.style.display = "none";
-    }   
+    }
+    
 });
+
+
+
+
